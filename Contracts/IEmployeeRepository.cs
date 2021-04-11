c8a6c1b4f6e7a6d9ab59fd99fa3d87040de917f9
Man Ioan <man_ioan@live.com>
2021-04-11 08:52:51 +0300

    Implement repository for Employees entity

---
+++ Contracts/IEmployeeRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IEmployeeRepository
+    {
+        IEnumerable<Employees> GetEmployees();
+        Employees GetEmployeeById(int EmployeeId);
+        void InsertEmployee(Employees Employee);
+        void DeleteEmployee(int EmployeeId);
+        void UpdateEmployee(Employees Employee);
+        void Save();
+    }
+}
---
+++ Repositories/EmployeeRepository.cs
@@ -0,0 +1,57 @@
+﻿
+using Contracts;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi.Models;
+
+namespace Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly MicroservicesContext _dbContext;
+
+        public EmployeeRepository(MicroservicesContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void DeleteEmployee(int EmployeeId)
+        {
+            var emp = _dbContext.Employees.Find(EmployeeId);
+            _dbContext.Employees.Remove(emp);
+            Save();
+        }
+
+        public Employees GetEmployeeById(int EmployeeId)
+        {
+            return _dbContext.Employees.Find(EmployeeId);
+        }
+
+        public IEnumerable<Employees> GetEmployees()
+        {
+            return _dbContext.Employees.ToList();
+        }
+
+        public void InsertEmployee(Employees Employee)
+        {
+            _dbContext.Add(Employee);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateEmployee(Employees Employee)
+        {
+            _dbContext.Entry(Employee).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
