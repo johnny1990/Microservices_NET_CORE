e3f47f37055112af72fdb9b0c43930b247fb208e
Man Ioan <man_ioan@live.com>
2021-04-18 20:51:00 +0300

    Implement operations for Departments

---
+++ Contracts/IDepartmentRepository.cs
@@ -0,0 +1,19 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    public interface IDepartmentRepository
+    {
+        IEnumerable<Departments> GetDepartments();
+        Departments GetDepartmentById(int DepartmentId);
+        void InsertDepartment(Departments Department);
+        void DeleteDepartment(int DepartmentId);
+        void UpdateDepartment(Departments Department);
+        void Save();
+    }
+}
---
+++ Repositories/DepartmentRepository.cs
@@ -0,0 +1,56 @@
+﻿using Contracts;
+using Database;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly MicroservicesContext _dbContext;
+
+        public DepartmentRepository(MicroservicesContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void DeleteDepartment(int DepartmentId)
+        {
+            var dep = _dbContext.Departments.Find(DepartmentId);
+            _dbContext.Departments.Remove(dep);
+            Save();
+        }
+
+        public Departments GetDepartmentById(int DepartmentId)
+        {
+            return _dbContext.Departments.Find(DepartmentId);
+        }
+
+        public IEnumerable<Departments> GetDepartments()
+        {
+            return _dbContext.Departments.ToList();
+        }
+
+        public void InsertDepartment(Departments Department)
+        {
+            _dbContext.Add(Department);
+            Save();
+        }
+
+        public void Save()
+        {
+            _dbContext.SaveChanges();
+        }
+
+        public void UpdateDepartment(Departments Department)
+        {
+            _dbContext.Entry(Department).State = EntityState.Modified;
+            Save();
+        }
+    }
+}
